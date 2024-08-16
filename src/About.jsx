ba57681dbaf06c33f23dfad69ac599508223c23f
Aaron Sharif <125632434+Sharif262@users.noreply.github.com>
2024-08-15 21:37:13 -0700

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -1,25 +1,34 @@
-// src/About.jsx
-
 import React from "react";
 import Navbar from "./Navbar";
+import "./About.css"; // Import the CSS file
 
 const About = () => {
   return (
-    <div>
+    <div className="about-page">
       <Navbar />
-      <div style={pageStyle}>
-        <h1>About Page</h1>
-        <p>This is the about page.</p>
+      <div className="about-container">
+        <div className="about-content">
+          <h1>Your ideal Travel Planner</h1>
+          <p>
+            Your all in one travel planner. Do you want to go on a vacation? Do
+            you want to meetup with friends somewhere, do you want to explore
+            attractions at a country. <strong>WayPoint</strong> has the ability
+            to make your travels convenient as you would like.
+          </p>
+          <h2>Why WayPoint?</h2>
+          <p>
+            Our objective is to make your travels easier, we want to save YOU
+            the hassle of having to call hotels for vacancy, to look for
+            restaurants that cost a lot of money, to overpay for a vacation
+            beyond your budget.
+          </p>
+        </div>
+        <div className="about-logo">
+          <h1>W</h1>
+        </div>
       </div>
     </div>
   );
-};
-
-const pageStyle = {
-  padding: "20px",
-  minHeight: "200vh",
-  backgroundColor: "#DADED4",
-  color: "#39603D",
 };
 
 export default About;
